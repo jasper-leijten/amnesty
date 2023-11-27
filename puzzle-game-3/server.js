67ddf90a9f521a8cfb19d28271b4998245bdb3c7
jasper-leijten <57407079+jasper-leijten@users.noreply.github.com>
2023-11-27 12:18:09 +0100

    changed port number

--- puzzle-game-3/server.js
+++ puzzle-game-3/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const port = 3000;
+const port = 4000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
